3bacf1639ba66bde8eee4e745ef64d9e965f8714
Tony Young <tony@rfw.name>
2014-02-09 10:44:02 +1300

    Log exceptions when a handler fails.

--- pydle/client.py
+++ pydle/client.py
@@ -357,8 +357,8 @@ class BasicClient:
             if not hasattr(self, method):
                 method = 'on_unknown'
             getattr(self, method)(message)
-        except:
-            self.logger.error('Failed to execute %s handler.', method)
+        except Exception as e:
+            self.logger.error('Failed to execute %s handler.', method, exc_info=e)
 
     def on_unknown(self, message):
         """ Unknown command. """
