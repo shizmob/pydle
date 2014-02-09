85039068de8d0091d23c09e1895bb0927b715eb0
Shiz <hi@shiz.me>
2014-02-09 02:11:39 +0100

    Add utility function to convert arbitrary names to Python identifiers.

--- pydle/features/ctcp.py
+++ pydle/features/ctcp.py
@@ -1,5 +1,6 @@
 ## ctcp.py
 # Client-to-Client-Protocol (CTCP) support.
+import pydle.protocol
 from pydle.features import rfc1459
 
 __all__ = [ 'CTCPSupport' ]
@@ -57,7 +58,7 @@ class CTCPSupport(rfc1459.RFC1459Support):
             type, contents = parse_ctcp(msg)
 
             # Find dedicated handler if it exists.
-            attr = 'on_ctcp_' + type.lower()
+            attr = 'on_ctcp_' + pydle.protocol.identifierify(type)
             if hasattr(self, attr):
                 getattr(self, attr)(nick, target, contents)
             else:
@@ -76,7 +77,7 @@ class CTCPSupport(rfc1459.RFC1459Support):
             type, response = parse_ctcp(msg)
 
             # Find dedicated handler if it exists.
-            attr = 'on_ctcp_' + type.lower() + '_reply'
+            attr = 'on_ctcp_' + pydle.protocol.identifierify(type) + '_reply'
             if hasattr(self, attr):
                 getattr(self, attr)(user, target, response)
             else:
