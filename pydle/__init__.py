181e11b4423d0ca2033528568cb311a1f180cccf
Shiz <hi@shiz.me>
2014-02-21 19:07:10 +0100

    Restrict API directly imported into the pydle namespace.

--- pydle/__init__.py
+++ pydle/__init__.py
@@ -1,8 +1,6 @@
 from . import async, connection, protocol, client, features
 
-from .async import EventLoop, Future, coroutine
-from .connection import NotConnected
-from .client import NotInChannel, AlreadyInChannel, BasicClient, ClientPool
+from .client import Error, NotInChannel, AlreadyInChannel, BasicClient, ClientPool
 from .features.ircv3_1.cap import NEGOTIATING as CAPABILITY_NEGOTIATING, FAILED as CAPABILITY_FAILED, NEGOTIATED as CAPABILITY_NEGOTIATED
 
 __name__ = 'pydle'
