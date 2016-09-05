41e5a549c24ef9324524522e139382edf36cc191
Ramiro Bou <bouramiro@gmail.com>
2016-09-04 21:13:51 -0300

    Sending WHOIS to new users if WHOX is unavailable.

--- pydle/features/whox.py
+++ pydle/features/whox.py
@@ -24,6 +24,11 @@ class WHOXSupport(isupport.ISUPPORTSupport, account.AccountSupport):
         else:
             # Find account name of person.
             pass
+    
+    def _create_user(self, nickname):
+        super()._create_user(nickname)
+        if self.registered and 'WHOX' not in self._isupport:
+            self.whois(nickname)
 
     def on_raw_354(self, message):
         """ WHOX results have arrived. """
