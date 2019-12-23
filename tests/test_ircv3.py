95aae28e5b6705061b5b4ee8c8ecc567d90036fb
Joshua Salzedo <thHunkn0WNd@gmail.com>
2019-12-22 16:19:02 -0800

    Handle IRCv3 tag escape sequences  fixes #123

---
+++ tests/test_ircv3.py
@@ -0,0 +1,24 @@
+import pytest
+
+from pydle.features import ircv3
+
+pytestmark = pytest.mark.new_test
+
+
+@pytest.mark.parametrize(
+    "payload, expected",
+    [
+        (
+                rb"@+example=raw+:=,escaped\:\s\\ :irc.example.com NOTICE #channel :Message",
+                {"+example": """raw+:=,escaped; \\"""}
+        ),
+        (
+            rb"@+example=\foo\bar :irc.example.com NOTICE #channel :Message",
+            {"+example": "foobar"}
+        ),
+    ]
+)
+def test_tagged_message_escape_sequences(payload, expected):
+    message = ircv3.tags.TaggedMessage.parse(payload)
+
+    assert message.tags == expected
