f4b3225efa2fd5817f219f1fa9f474e38aa7dae9
Joshua Salzedo <thHunkn0WNd@gmail.com>
2020-02-09 22:59:08 -0800

    v0.9.4rc1

--- pydle/__init__.py
+++ pydle/__init__.py
@@ -8,8 +8,8 @@ from .features.ircv3.cap import NEGOTIATING as CAPABILITY_NEGOTIATING, FAILED as
 from asyncio import coroutine, Future
 
 __name__ = 'pydle'
-__version__ = '0.9.3'
-__version_info__ = (0, 9, 3)
+__version__ = '0.9.4rc1'
+__version_info__ = (0, 9, 4)
 __license__ = 'BSD'
 
 
